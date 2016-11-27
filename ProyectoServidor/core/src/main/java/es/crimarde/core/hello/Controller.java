108ab026e7a69774bfcabe0ff4bccaa61481c048
Cristobal Martinez <insachris@gmail.com>
2016-11-27 04:32:20 +0100

    Metodos para el controlador. De momento genericos. Pero ya preparado todo el crud. Responden a los test de SOAPUI

--- ProyectoServidor/core/src/main/java/es/crimarde/core/hello/Controller.java
+++ ProyectoServidor/core/src/main/java/es/crimarde/core/hello/Controller.java
@@ -1,6 +1,7 @@
 package es.crimarde.core.hello;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,29 @@ import es.crimarde.service.Servicio;
 public class Controller {
 	
 	@Autowired Servicio servicio;
-	
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     
-    @RequestMapping("/test")
-    public TestResponse lectura(@RequestParam(value="name", defaultValue="Usuario") String name) {
-        return new TestResponse(String.format(template, name), Status.SUCCESS.getDesc());
+    @RequestMapping("/lista")
+    public List<TestResponse> retrieveList() {
+        return null;
+    }
+    
+    @RequestMapping("/add")
+    public List<String> add(@RequestParam(value="name", defaultValue="Usuario") String name) {
+        return Arrays.asList("add");
+    }
+    
+    @RequestMapping("/delete")
+    public String delete(@RequestParam(value="name", defaultValue="Usuario") String name) {
+        return ("deleted");
+    }
+    
+    @RequestMapping("/edit")
+    public String edit(@RequestParam(value="name", defaultValue="Usuario") String name) {
+    	return ("edited");
+    }
+    
+    @RequestMapping("/retrieve")
+    public String retrieve(@RequestParam(value="name", defaultValue="Usuario") String name) {
+    	return ("retrieve");
     }
 }
