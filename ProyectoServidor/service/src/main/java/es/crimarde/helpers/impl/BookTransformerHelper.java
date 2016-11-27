26b7ae47a16182d3b9589c03f2717a1df0e2b110
Cristobal Martinez <insachris@gmail.com>
2016-11-27 23:50:29 +0100

    Modificaciones en el transformer de libro

--- ProyectoServidor/service/src/main/java/es/crimarde/helpers/impl/BookTransformerHelper.java
+++ ProyectoServidor/service/src/main/java/es/crimarde/helpers/impl/BookTransformerHelper.java
@@ -14,21 +14,16 @@ import ma.glasnost.orika.MapperFacade;
 @Component
 public class BookTransformerHelper implements BookTransformerHelp {
 
-	public BookDTO toDTO(Book book){
+	public BookDTO entityToDto(Book book){
 		MapperFacade mapper = OrikaMapper.getMapperFacade();
-		BookDTO dto = mapper.map(book, BookDTO.class);		
-		return dto;
+		return mapper.map(book, BookDTO.class);		
 	}
 	
-	public Book toEntity (BookDTO bookDTO){	//Por hacer
+	public Book dtoToEntity (BookDTO bookDTO){	//Por hacer
 		MapperFacade mapper = OrikaMapper.getMapperFacade();
-		Book entity = mapper.map(bookDTO, Book.class);
-		return entity;
+		return mapper.map(bookDTO, Book.class);
 	}
 	
-	/**
-	 * comentario
-	 */
 	public List<Book> dtoToEntityList(List<BookDTO> bookDtoList){
 		List<Book> bookList = new ArrayList<>();
 		
@@ -39,7 +34,7 @@ public class BookTransformerHelper implements BookTransformerHelp {
 		return bookList;
 	}
 	
-	public List<BookDTO> EntityToDtoList(List<Book> bookList){
+	public List<BookDTO> entityToDtoList(List<Book> bookList){
 		List<BookDTO> bookDTOList = new ArrayList<>();
 		
 		for (Book Book : bookList) {
@@ -49,65 +44,4 @@ public class BookTransformerHelper implements BookTransformerHelp {
 		return bookDTOList;
 	}
 
-
-	@Override
-	public BookDTO entityToDto(Book employee) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public Book dtoToEntity(BookDTO bookDTO) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-//	public Book dtoToEntity(BookDTO bookDTO){
-//		Book Book = null;
-//		if(bookDTO != null){
-//			Book = new Book();
-//			if(bookDTO.getEmail() != null){
-//				Book.setEmail(bookDTO.getEmail());
-//			}
-//			if(bookDTO.getFirstName() != null){
-//				Book.setFirstName(bookDTO.getFirstName());
-//			}
-//			Book.setId(new Long(bookDTO.getId()));
-//			if(bookDTO.getLastName() != null){
-//				Book.setLastName(bookDTO.getLastName());
-//			}
-//			if(bookDTO.getPhone() != null){
-//				Book.setPhone(bookDTO.getPhone());
-//			}
-//			if(bookDTO.getEmail() != null){
-//				Book.setEmail(bookDTO.getEmail());
-//			}
-//		}
-//		return Book;
-//	}
-//	
-//	public BookDTO entityToDto(Book Book){
-//		BookDTO employeeDTO = null;
-//		if(Book != null){
-//			employeeDTO = new BookDTO();
-//			if(Book.getEmail() != null){
-//				employeeDTO.setEmail(Book.getEmail());
-//			}
-//			if(Book.getFirstName() != null){
-//				employeeDTO.setFirstName(Book.getFirstName());
-//			}
-//			employeeDTO.setId(new Long(Book.getId()));
-//			if(Book.getLastName() != null){
-//				employeeDTO.setLastName(Book.getLastName());
-//			}
-//			if(Book.getPhone() != null){
-//				employeeDTO.setPhone(Book.getPhone());
-//			}
-//			if(Book.getEmail() != null){
-//				employeeDTO.setEmail(Book.getEmail());
-//			}
-//		}
-//		return employeeDTO;
-//	}
 }
