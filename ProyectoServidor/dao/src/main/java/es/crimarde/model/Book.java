6648e47a63bc6995b28c4a7fe5cdc20418f4cd80
Cristobal Martinez <insachris@gmail.com>
2016-11-27 19:14:51 +0100

    Añadido patron builder en la clase Book

--- ProyectoServidor/dao/src/main/java/es/crimarde/model/Book.java
+++ ProyectoServidor/dao/src/main/java/es/crimarde/model/Book.java
@@ -6,21 +6,43 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.mysql.jdbc.Blob;
+
 @Entity
 @Table(name = "Book")
 
 public class Book {
 
-	@Id
+	
 	@GeneratedValue
-	private int id;
-	@Column(name="name")
-	private String name;
-	@Column(name="pages")
-	private String pages;
+	@Id 						private int id;
+	@Column(name="titulo") 		private String titulo;
+	@Column(name="autor")  		private String autor;
+	@Column(name="sinopsis")	private String sinopsis;
+	@Column(name="imagen")		private Blob imagen;
+	@Column(name="precio")		private double precio;
 
 	public Book() {
-		// TODO Auto-generated constructor stub
+	}
+		
+	public Book(int id, String titulo, String autor, String sinopsis, Blob imagen, double precio) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.sinopsis = sinopsis;
+		this.imagen = imagen;
+		this.precio = precio;
+	}
+	
+	public Book(Builder builder) {
+		super();
+		this.id = builder.getId();
+		this.titulo = builder.getTitulo();
+		this.autor = builder.getAutor();
+		this.sinopsis = builder.getSinopsis();
+		this.imagen = builder.getImagen();
+		this.precio = builder.getPrecio();
 	}
 
 	public int getId() {
@@ -31,21 +53,109 @@ public class Book {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getTitulo() {
+		return titulo;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 
-	public String getPages() {
-		return pages;
+	public String getAutor() {
+		return autor;
 	}
 
-	public void setPages(String pages) {
-		this.pages = pages;
+	public void setAutor(String autor) {
+		this.autor = autor;
 	}
 
+	public String getSinopsis() {
+		return sinopsis;
+	}
 
+	public void setSinopsis(String sinopsis) {
+		this.sinopsis = sinopsis;
+	}
+
+	public Blob getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(Blob imagen) {
+		this.imagen = imagen;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
+
+class Builder {
+	
+	private int id;
+	private String titulo;
+	private String sinopsis;
+	private String autor;
+	private Blob imagen;
+	private double precio;
+
+	public Builder() {
+	}
+
+	public void whithId(int id) {
+		this.id = id;
+	}
+
+	public void whithTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void whithSinopsis(String sinopsis) {
+		this.sinopsis = sinopsis;
+	}
+
+	public void whithAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public void whithImagen(Blob imagen) {
+		this.imagen = imagen;
+	}
+
+	public void whithPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	public Book build(){
+		return new Book(this);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getSinopsis() {
+		return sinopsis;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public Blob getImagen() {
+		return imagen;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+	
 }
