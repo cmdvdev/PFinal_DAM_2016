8bd4a7ccd9b26459e569faf593a26a984536c8fe
Cristobal Martinez <insachris@gmail.com>
2016-11-27 04:51:56 +0100

    Creacion de objetos de capa de servicio

---
+++ ProyectoServidor/service/src/main/java/es/crimarde/helpers/EmployeeTransformerHelp.java
@@ -0,0 +1,13 @@
+package es.crimarde.helpers;
+
+import java.util.List;
+
+import es.crimarde.negocio.EmployeeDTO;
+
+
+public interface EmployeeTransformerHelp {
+	public EmployeeDTO entityToDto(Employee employee);
+	public Employee dtoToEntity(EmployeeDTO employeeDTO);	
+	public List<EmployeeDTO> EntityToDtoList(List<Employee> list);
+	public List<Employee> dtoToEntityList(List<EmployeeDTO> dtoList);
+}
