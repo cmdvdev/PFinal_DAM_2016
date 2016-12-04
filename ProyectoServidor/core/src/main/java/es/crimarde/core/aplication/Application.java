b4c75eeeec77599e40d22e767b98c6d49c63fdce
Cristobal Martinez <insachris@gmail.com>
2016-12-04 17:37:24 +0100

    Probando logs en el arranque del programa

--- ProyectoServidor/core/src/main/java/es/crimarde/core/aplication/Application.java
+++ ProyectoServidor/core/src/main/java/es/crimarde/core/aplication/Application.java
@@ -1,5 +1,7 @@
 package es.crimarde.core.aplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
@@ -19,7 +21,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("es.crimarde")
 public class Application {
 	
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        //Pruebas con el log de consola
+        logger.error("Message logged at ERROR level");
+        logger.warn("Message logged at WARN level");
+        logger.info("Message logged at INFO level");
+        logger.debug("Message logged at DEBUG level");
+
     }
 }
